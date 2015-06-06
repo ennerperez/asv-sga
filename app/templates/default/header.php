34651c427485024ba478ce38a7c8bde8f3fb2452
Enner Pérez <ennerperez@gmail.com>
2015-06-05 19:45:04 -0430

    Favicon ajustment

--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -18,7 +18,16 @@ $hooks = Hooks::get();
 	$hooks->run('meta');
 	?>
 	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in app/Core/Config.php ?></title>
-	<link rel="icon" href="favicon.ico">
+	
+	<!-- Favicon -->
+	<!-- For IE 9 and below. ICO should be 32x32 pixels in size -->
+	<!--[if IE]><link rel="shortcut icon" href="favicon.ico"><![endif]-->
+
+	<!-- Touch Icons - iOS and Android 2.1+ 180x180 pixels in size. --> 
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+
+	<!-- Firefox, Chrome, Safari, IE 11+ and Opera. 196x196 pixels in size. -->
+	<link rel="icon" href="favicon.png">
 
 	<!-- CSS -->
 	<?php
